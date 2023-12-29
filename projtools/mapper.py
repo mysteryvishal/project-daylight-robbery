b8b2eedbc0e99431679e5760f9a57d31c73d8831
Vishal Mistry <vishalmistry51@gmail.com>
2023-12-29 16:16:28 +0000

    updated mapper function

--- projtools/mapper.py
+++ projtools/mapper.py
@@ -34,7 +34,7 @@ class Mapper:
 
     def make_map(self):
 
-        vmin, vmax = 120, 220
+        vmin, vmax = self.df[self.variable].min(), self.df[self.variable].max()
         fig, ax = plt.subplots(1, figsize=(10, 6))
 
         # merging the dataframes
